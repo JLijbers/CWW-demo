ac25eef029d44dd0cd2475ec28ec7ee8e7aab188
JLijbers <joannelijbers@gmail.com>
2023-10-13 18:17:04 +0200

    Streamlit app
    Second version, turned it into streamlit application. Still needs improvement with regards to RAG.

--- main.py
+++ main.py
@@ -1,39 +1,76 @@
-from langchain.text_splitter import TokenTextSplitter
-from langchain.chat_models import ChatOpenAI
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.indexes import VectorstoreIndexCreator
-from loader.JSONLoader import JSONLoader
+import os
+import streamlit as st
 from dotenv import load_dotenv
+
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
+from langchain.vectorstores import Chroma
 
 # Load environment variables from .env file
 load_dotenv()
+website_url = os.environ.get('WEBSITE_URL', 'een website')
 
-# Load crawled data
-loader = JSONLoader(
-    file_path='data/scraped_data.json',
-    embed_keys='.text[]',
-    metadata_keys='.metadata[]')
+# Streamlit settings
+st.set_page_config(
+    page_title=f'Stel vragen aan: {website_url}',
+    page_icon="⚽",)
+st.title('⚽ Chat met: Korfbaldws.nl')
 
-data = loader.load()
 
-# Create a vector store based on the data
-index = VectorstoreIndexCreator(
-    # split the documents into chunks
-    text_splitter=TokenTextSplitter(chunk_size=1000, chunk_overlap=100),
-    # select embeddings
-    embedding=OpenAIEmbeddings(),
-    # set vectorstore
-    vectorstore_cls=Chroma
-).from_loaders([loader])
+@st.cache_resource(ttl='1h')
+# Supporting function and class
+def get_retriever():
+    embeddings = OpenAIEmbeddings()
+    vectordb = Chroma(persist_directory='db', embedding_function=embeddings)
+
+    retriever = vectordb.as_retriever(search_type='mmr', k=3)
+
+    return retriever
+
+
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container: st.delta_generator.DeltaGenerator, initial_text: str = ''):
+        self.container = container
+        self.text = initial_text
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token
+        self.container.markdown(self.text)
+
+
+# Start
+# Set Chroma vector DB as retriever
+retriever = get_retriever()
+
+# Set use of chat history
+msgs = StreamlitChatMessageHistory(key='langchain_messages')
+memory = ConversationBufferMemory(memory_key='chat_history', chat_memory=msgs, return_messages=True)
 
 # Initialize the ChatOpenAI model
-llm = ChatOpenAI(model="gpt-3.5-turbo-16k", temperature=0.1)
+llm = ChatOpenAI(model="gpt-3.5-turbo-16k", temperature=0.1, streaming=True)
 
-# Query the vector store
-# While loop to keep asking questions
-while True:
-    query = input("What is your question? ")
-    result = index.query(llm=llm, question=query, chain_type="stuff")
-    print(result)
-    print("------\n")
+# Create the Q&A chain
+qa_chain = ConversationalRetrievalChain.from_llm(
+    llm=llm, retriever=retriever, memory=memory, verbose=False
+)
+
+# Streamlit settings
+if st.sidebar.button('Berichtgeschiedenis wissen') or len(msgs.messages) == 0:
+    msgs.clear()
+    msgs.add_ai_message(f'Vraag me alles over: {website_url}!')
+
+avatars = {'human': 'user', 'ai': 'assistant'}
+for msg in msgs.messages:
+    st.chat_message(avatars[msg.type]).write(msg.content)
+
+if user_query := st.chat_input(placeholder='Wat wil je weten over de mooiste korfbalvereniging van Nederland?'):
+    st.chat_message('user').write(user_query)
+
+    with st.chat_message('assistant'):
+        stream_handler = StreamHandler(st.empty())
+        # Run the Q&A chain
+        response = qa_chain.run(user_query, callbacks=[stream_handler])
--- scrape.py
+++ scrape.py
@@ -1,18 +1,43 @@
-from langchain.docstore.document import Document
-from langchain.utilities import ApifyWrapper
-from dotenv import load_dotenv
+import os
 
-# Load environment variables from .env file
+from apify_client import ApifyClient
+from dotenv import load_dotenv
+from langchain.document_loaders import ApifyDatasetLoader
+from langchain.document_loaders.base import Document
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+
+# Load environment variables from a .env file
 load_dotenv()
 
-apify = ApifyWrapper()
+if __name__ == '__main__':
+    apify_client = ApifyClient(os.environ.get('APIFY_API_TOKEN'))
+    website_url = os.environ.get('WEBSITE_URL')
+    print(f'Extracting data from "{website_url}". Please wait...')
+    actor_run_info = apify_client.actor('apify/website-content-crawler').call(
+        run_input={'startUrls': [{'url': website_url}]}
+    )
 
-# Call the Actor to obtain text from the crawled webpages
-run = apify.call_actor(
-    actor_id="apify/website-content-crawler",
-    run_input={"startUrls": [{"url": "https://www.korfbaldws.nl/"}]},
-    dataset_mapping_function=lambda item: Document(
-        page_content=item["text"] or "", metadata={"source": item["url"]}
-    ),
-)
+    print('Saving data into the vector database. Please wait...')
+    loader = ApifyDatasetLoader(
+        dataset_id=actor_run_info['defaultDatasetId'],
+        dataset_mapping_function=lambda item: Document(
+            page_content=item['text'] or '', metadata={'source': item['url']}
+        ),
+    )
+
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=100)
+    docs = text_splitter.split_documents(documents)
+
+    embedding = OpenAIEmbeddings()
+
+    vectordb = Chroma.from_documents(
+        documents=docs,
+        embedding=embedding,
+        persist_directory='db',
+    )
+    vectordb.persist()
+    print('All done!')
 
